b438c020211efa31763310b5f440f69c10eac71b
samukelolukhele <samukelo.business@gmail.com>
2023-03-08 15:46:26 +0200

    First Commit

---
+++ src/components/pages/Home/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-[90vh] bg-black">
+      <div className="container">
+        <div className="w-[460px] flex flex-column gap-4">
+          <h1 className="text-6xl ">Bring Your Vision To Life.</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
