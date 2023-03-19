0d777b12f91cf3e817a6b3828f2864b3909101ed
samukelolukhele <samukelo.business@gmail.com>
2023-03-19 15:23:41 +0200

    FEAT: Home Side Section is now responsive.

--- src/components/pages/Home/SideSection.tsx
+++ src/components/pages/Home/SideSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type SProps = {
-  direction: "flex-row" | "flex-row-reverse";
+  direction: "md:flex-row" | "md:flex-row-reverse";
   header: string;
   body: string;
   image: string;
@@ -11,14 +11,18 @@ const SideSection = ({ direction, header, body, image }: SProps) => {
   return (
     <div className="py-10">
       <div
-        className={`container flex ${direction} gap-[10rem] justify-between`}
+        className={`container flex flex-col-reverse ${direction} gap-10 md:gap-[10rem] items-center md:items-start justify-between`}
       >
-        <div className="flex flex-col gap-4 w-[300px] my-auto">
+        <div className="flex flex-col items-center md:items-start text-center md:text-center gap-4 w-[300px] my-auto">
           <h3 className="text-5xl">{header}</h3>
           <hr className="w-full border-[1.5px] border-bDark mb-4" />
           <p className="text-tSecondary text-xl">{body}</p>
         </div>
-        <img className="bg-center bg-cover w-full h-full" src={image} alt="" />
+        <img
+          className="bg-center object-cover w-full h-[50vh] md:h-full"
+          src={image}
+          alt=""
+        />
       </div>
     </div>
   );
