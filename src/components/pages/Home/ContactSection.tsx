0a628ebb4dbf325e92aeb2594ee7a1d9dff4c758
samukelolukhele <samukelo.business@gmail.com>
2023-08-19 16:40:31 +0200

    FIX: Home mobile spacing issues resolved.

--- src/components/pages/Home/ContactSection.tsx
+++ src/components/pages/Home/ContactSection.tsx
@@ -6,11 +6,11 @@ const ContactSection = () => {
   return (
     <div className="relative flex justify-center items-center w-full h-[350px] bg-fixed bg-[url(/images/Home-Contact.jpg)] bg-cover bg-center bg-no-repeat">
       <div className="absolute bg-black w-full h-full opacity-80"></div>
-      <div className="relative flex flex-col items-center gap-4 z-2 text-white">
-        <h4 className="text-4xl w-[600px] text-center">
+      <div className="relative flex flex-col items-center gap-2 md:gap-4 z-2 text-white mx-6 md:mx-0">
+        <h4 className="text-2xl md:text-4xl md:w-[600px] text-center">
           Book an appointment with us.
         </h4>
-        <Divider width="w-4/12" customStyling="mx-auto" />
+        <Divider width="w-4/12" customStyling="m-auto" />
         <p className="text-tDark text-center text-xl">
           We look forward to hearing from you
         </p>
